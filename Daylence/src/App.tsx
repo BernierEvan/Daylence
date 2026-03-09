d543e92e4e96b545591aaf995e8500b9a2212c71
Evan Bernier <bernier.evan.pro@gmail.com>
2026-03-09 09:08:13 +0100

    Fix merge conflicts and tidy up App.tsx
    Removed merge conflict markers and cleaned up imports.

--- Daylence/src/App.tsx
+++ Daylence/src/App.tsx
@@ -1,10 +1,5 @@
 import "./App.css";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> Stashed changes
 import LandingPage from "./features/landing/pages/LandingPage";
 import DiscoverPage from "./resources/discover/pages/DiscoverPage";
 import SupportPage from "./resources/support/SupportPage";
@@ -15,28 +10,9 @@ import SettingsPage from "./features/settings/pages/SettingsPage";
 import ProfilePage from "./features/settings/pages/ProfilePage";
 import BudgetPage from "./features/budget/pages/BudgetPage";
 
-function App() {
-  return (
-<<<<<<< Updated upstream
-    <>
-      <LandingPage />
-    </>
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./features/landing/pages/LandingPage";
-import DiscoverPage from "./resources/discover/pages/DiscoverPage";
-import SupportPage from "./resources/support/SupportPage";
-import CguPage from "./policies/cgu/pages/CguPage";
-import ConfidentialityPage from "./policies/confidentiality/pages/ConfidentialityPage";
-import TransportPage from "./features/transport/pages/TransportPage";
-import SettingsPage from "./features/settings/pages/SettingsPage";
-import ProfilePage from "./features/settings/pages/ProfilePage";
-import BudgetPage from "./features/budget/pages/BudgetPage";
-//imports
 
 function App() {
   return (
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./features/landing/pages/LandingPage";
 import DiscoverPage from "./resources/discover/pages/DiscoverPage";
@@ -47,7 +23,6 @@ import TransportPage from "./features/transport/pages/TransportPage";
 import SettingsPage from "./features/settings/pages/SettingsPage";
 import ProfilePage from "./features/settings/pages/ProfilePage";
 import BudgetPage from "./features/budget/pages/BudgetPage";
-//imports
 
 function App() {
   return (
